1afc6d9ee8ada5b9b6a778512528df8ae402ad27
Mohamed Ismail <ismailreality1@gmail.com>
2024-04-09 14:35:29 +0200

    feat(config): create wallet + keys + account in config init command

---
+++ packages/tools/kadena-cli/src/wallets/utils/walletHelpers.ts
@@ -0,0 +1,65 @@
+import path from 'node:path';
+import { writeAccountAliasMinimal } from '../../account/utils/createAccountConfigFile.js';
+import { log } from '../../utils/logger.js';
+import { relativeToCwd } from '../../utils/path.util.js';
+
+export const logWalletInfo = (
+  words: string,
+  filepath: string,
+  publicKey: string,
+): void => {
+  log.info(log.generateTableString(['Mnemonic Phrase'], [[words]]));
+  log.info(
+    log.color.yellow(
+      '\nPlease store the mnemonic phrase in a safe place. You will need it to recover your wallet.\n',
+    ),
+  );
+  log.info(log.color.green('First keypair generated'));
+  log.info(`publicKey: ${publicKey}\n`);
+  log.info(
+    log.generateTableString(
+      ['Wallet Storage Location'],
+      [[relativeToCwd(filepath)]],
+    ),
+  );
+};
+
+export const logAccountCreation = (
+  accountName: string,
+  filePath: string,
+): void => {
+  log.info(log.color.green(`Account created`));
+  log.info(`accountName: ${accountName}\n`);
+  log.info(
+    log.generateTableString(
+      ['Account Storage Location'],
+      [[relativeToCwd(filePath)]],
+    ),
+  );
+};
+
+export const createAccountAliasByPublicKey = async (
+  alias: string,
+  publicKey: string,
+  directory: string,
+): Promise<{
+  accountName: string;
+  accountFilepath: string;
+}> => {
+  const accountName = `k:${publicKey}`;
+  const accountFilepath = path.join(directory, `${alias}.yaml`);
+  await writeAccountAliasMinimal(
+    {
+      accountName,
+      fungible: 'coin',
+      predicate: `keys-all`,
+      publicKeysConfig: [publicKey],
+    },
+    accountFilepath,
+  );
+
+  return {
+    accountName,
+    accountFilepath,
+  };
+};
